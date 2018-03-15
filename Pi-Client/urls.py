74070c93a0908f573d04afe7c504119c6e6f0720
venom <vasuratanpara@gmail.com>
2018-03-16 01:35:48 +0530

    first commit

---
+++ Pi-Client/urls.py
@@ -0,0 +1,6 @@
+URL_REG = 'http://localhost/PyHomeAutomation/api/reg_pi.php?'
+URL_LOGIN = 'http://localhost/PyHomeAutomation/api/login.php?'
+
+#URL_REG = 'http://pyhomeautomation.uphero.com/api/reg_pi.php?'
+#URL_LOGIN = 'http://pyhomeautomation.uphero.com/api/login.php?'
+#URL_GET = 'http://pyhomeautomation.uphero.com/api/get.php?'
---
+++ README.md
@@ -0,0 +1,6 @@
+# Py-Home-Automation
+http://pyhomeautomation.uphero.com/
+
+
+
+![Py-Home-Automation](https://github.com/vasuratanpara/Py-Home-Automation/blob/master/main-screen.png)
