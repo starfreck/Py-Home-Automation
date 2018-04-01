d4c6984617d31bbb408efef2d1060f44c1440155
Vasu Ratanpara <vasuratanpara@gmail.com>
2018-04-01 10:35:31 +0530

    Android-App Added Again..

---
+++ Android-App/app/src/main/java/com/vasu/venom/afinal/app/AppConfig.java
@@ -0,0 +1,23 @@
+package com.vasu.venom.afinal.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://192.168.43.186/PyHomeAutomation/api/login.php?";
+//	public static String URL_LOGIN = "http://pyhomeautomation.uphero.com/api/login.php?";
+	//public static String URL_LOGIN = "http://192.168.43.16/ha/api/login.php?";
+
+
+	// Server user register url
+	public static String URL_REGISTER = "http://192.168.43.186/PyHomeAutomation/api/register.php";
+//	public static String URL_REGISTER = "http://pyhomeautomation.uphero.com/api/register.php";
+	//public static String URL_REGISTER = "http://192.168.43.16/ha/api/register.php";
+
+	// Server user android to pi url
+	public static String URL_Android_pi = "http://192.168.43.186/PyHomeAutomation/api/Android_Pi.php?";
+//	public static String URL_Android_pi = "http://pyhomeautomation.uphero.com/api/Android_Pi.php?";
+	//public static String URL_Android_pi = "http://192.168.43.16/ha/api/Android_Pi.php?";
+
+	// Server user GET testing url
+	//public static String URL_GET = "http://pyhomeautomation.uphero.com/api/get.php?";
+
+}
